f7c64a8b3cc146b4d52054a83c836c550838305d
Adrian Cuello <152096555+Picsaboy@users.noreply.github.com>
2024-07-29 20:34:08 -0400

    Upload Proyecto Completo
    Upload Proyecto Completo. Espero revision.

---
+++ autores.php
@@ -0,0 +1,30 @@
+<?php
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'dblibreria';
+
+
+try {
+    $dsn = "mysql:host=$servername;dbname=$dbname;charset=utf8mb4";
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ];
+    $pdo = new PDO($dsn, $username, $password, $options);
+    
+    // La conexión fue exitosa, pero ya no mostramos el mensaje
+} catch (PDOException $e) {
+    // En un entorno de producción, es mejor no mostrar detalles del error
+    die('Error de conexión: ' . $e->getMessage());
+}
+
+
+// Consulta para obtener la lista de autores
+$stmt = $pdo->query('SELECT DISTINCT apellido, pais, nombre FROM autores');
+$autores = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<?php include 'index.php'; ?>
+<!-- Contenido específico de la página autores -->
